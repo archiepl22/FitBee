f789a050e67317055357b2a9fb4c8ccf03c2c41f
Magitron <maggiecaspar@gmail.com>
2017-01-19 18:15:56 +0700

    more styles and js for header nav

---
+++ assets/javascripts/header.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+  $(".c-nav-primary").find('> li').click( function(event){
+      event.stopPropagation();
+      $(this).find('> ul').toggle();
+  });
+
+  $(document).click( function(){
+      $('.c-nav-primary__item-nav').hide();
+  });
+});
+
+
+
+
+
+
