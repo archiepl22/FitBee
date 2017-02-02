5e38c606c0beef5ab4810a503ee4a2faa658136f
Magitron <maggiecaspar@gmail.com>
2017-02-02 18:28:36 +0700

    add and style profile components

--- dist/js/header.js
+++ dist/js/header.js
@@ -1,6 +1,5 @@
 $( document ).ready(function() {
   $(".c-nav-primary").find('> li').click( function(event){
-    console.log('meow');
       event.stopPropagation();
       $(this).find('> ul').toggle();
   });
@@ -9,16 +8,34 @@ $( document ).ready(function() {
       $('.c-nav-primary__item-nav').hide();
   });
 
+  // Tab-bar mobile dashboard menu
+  // ---
+
   $(".c-tab-bar__dash").click( function(){
-    console.log('meow');
     $(".c-mobile-dash").addClass('c--open');
   });
 
+  // close on back button
   $(".c-mobile-dash__back").click( function(){
     $(".c-mobile-dash").removeClass('c--open');
   });
 
+  // close on link click
   $(".c-mobile-dash").find("li").click( function(){
     $(".c-mobile-dash").removeClass('c--open'); 
+  });
+
+
+  // Profile feed
+  // ---
+
+  $(".js-profile-about").click( function(){
+     $('.js-feed').removeClass('c--active');
+     $('.js-about').addClass('c--active');
+  });
+
+  $(".js-profile-feed").click( function(){
+     $('.js-about').removeClass('c--active');
+     $('.js-feed').addClass('c--active');
   });
 });
