caa636dee95f2aa33bcdc1d55d7e05b9fa28f2ea
Magitron <maggiecaspar@gmail.com>
2017-02-14 14:21:19 +0700

    styled mobile messages

--- dist/js/header.js
+++ dist/js/header.js
@@ -70,6 +70,7 @@ $( document ).ready(function() {
     $('.t-messages__conversation-new').addClass('active');
     $('.js-messages').children().hide();
     $(".js-contact").not(this).removeClass('active');
+    $('.js-contacts').removeClass('active')
     $('.js-contact.new').addClass('active')
 
   });
@@ -78,6 +79,11 @@ $( document ).ready(function() {
     $('.t-messages__conversation-new').removeClass('active');
     $('.t-messages__conversation-name').addClass('active');
     $('.js-messages').children().show();
+    $('.js-contacts').removeClass('active')
+  });
+
+  $('.js-messages-back').click(function(){
+    $('.js-contacts').addClass('active')
   });
 });
 
